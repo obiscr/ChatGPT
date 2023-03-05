662189dbd2f81c8ad532e6840605d8c1206ecfe6
Wuzi <wuzi@obiscr.com>
2023-03-05 16:47:13 +0800

    Add Support/Donate info

--- src/main/java/com/obiscr/chatgpt/icons/ChatGPTIcons.java
+++ src/main/java/com/obiscr/chatgpt/icons/ChatGPTIcons.java
@@ -14,5 +14,9 @@ public class ChatGPTIcons {
     public static final Icon AI = IconLoader.getIcon("/images/ai.png",ChatGPTIcons.class);
 
     public static final Icon OPEN_AI = IconLoader.getIcon("/images/openai.png",ChatGPTIcons.class);
+    public static final Icon SUPPORT = IconLoader.getIcon("/icons/support.svg",ChatGPTIcons.class);
+    public static final Icon WE_CHAT = IconLoader.getIcon("/images/wx_pay.png",ChatGPTIcons.class);
+    public static final Icon ALIPAY = IconLoader.getIcon("/images/ali_pay.png",ChatGPTIcons.class);
+    public static final Icon BUE_ME_A_COFFEE = IconLoader.getIcon("/images/coffee.png",ChatGPTIcons.class);
 
 }
