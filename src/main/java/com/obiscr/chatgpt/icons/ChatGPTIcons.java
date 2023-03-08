29c874a5740b98f0e69ead5eb848a4a3462d019b
Wuzi <wuzi@obiscr.com>
2023-03-08 19:09:47 +0800

    Support for modifying sent text

--- src/main/java/com/obiscr/chatgpt/icons/ChatGPTIcons.java
+++ src/main/java/com/obiscr/chatgpt/icons/ChatGPTIcons.java
@@ -18,6 +18,7 @@ public class ChatGPTIcons {
     public static final Icon WE_CHAT = IconLoader.getIcon("/images/wx_pay.png",ChatGPTIcons.class);
     public static final Icon ALIPAY = IconLoader.getIcon("/images/ali_pay.png",ChatGPTIcons.class);
     public static final Icon BUE_ME_A_COFFEE = IconLoader.getIcon("/images/coffee.png",ChatGPTIcons.class);
+    public static final Icon PAY_PAL = IconLoader.getIcon("/images/paypal.png",ChatGPTIcons.class);
     public static final Icon TOOL_WINDOW = IconLoader.getIcon("/icons/toolWindow.svg",ChatGPTIcons.class);
 
 }
