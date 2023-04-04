11940b2e7301f43591e3dc1205ecc326e11968dc
xuhengjin <xuhengjin@bytedance.com>
2023-04-04 20:51:03 +0800

    Feature: get inputfocus in ChatGPT-3.5-Turbo tab
     get input focus rapidly by keystroke F key

--- src/main/java/com/obiscr/chatgpt/GPT35TurboToolWindow.java
+++ src/main/java/com/obiscr/chatgpt/GPT35TurboToolWindow.java
@@ -5,6 +5,8 @@ import com.obiscr.chatgpt.ui.MainPanel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 
 /**
@@ -24,5 +26,24 @@ public class GPT35TurboToolWindow {
 
   public MainPanel getPanel() {
     return panel;
+  }
+
+/**
+ * rapidly get input focus by keystorke f key
+ */
+  public void registerKeystrokeFocus(){
+    JTextArea myTextArea = panel.getSearchTextArea().getTextArea();
+    myTextArea.setFocusable(true);
+    Action focusAction = new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        myTextArea.requestFocus();
+      }
+    };
+
+    KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_F,0);
+    InputMap inputMap = myTextArea.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+    inputMap.put(keyStroke, "focusAction");
+    myTextArea.getActionMap().put("focusAction", focusAction);
   }
 }
